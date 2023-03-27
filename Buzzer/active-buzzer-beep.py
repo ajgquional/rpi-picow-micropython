b6a8dab0081a0e3d3b59b85b10f082e5f795a833
Adrian Josele Quional <13906452+ajgquional@users.noreply.github.com>
2023-03-27 18:48:43 +0800

    Add files via upload

---
+++ Buzzer/active-buzzer-beep.py
@@ -0,0 +1,21 @@
+# Project objective: To test an active buzzer to play an alarm sound at one second interval
+#
+# Hardware and connections used:
+#   Active buzzer GND to Raspberry Pi Pico GND
+#	Active buzzer + Pin to GPIO Pin 15
+#
+# Programmer: Adrian Josele G. Quional
+
+# if active buzzer is used, import the Buzzer class from picozero
+from picozero import Buzzer
+from time import sleep
+
+# creating a Buzzer object
+buzzer = Buzzer(15)
+
+# continuously beep at 1 sec interval while the board has power
+while True:
+    buzzer.on()
+    sleep(1)
+    buzzer.off()
+    sleep(1)
---
+++ Buzzer/passive-buzzer-beep.py
@@ -0,0 +1,22 @@
+# Project objective: To test a passive buzzer to play an alarm sound at one second interval
+#
+# Hardware and connections used:
+#   Passive buzzer GND to Raspberry Pi Pico GND
+#	Passive buzzer + Pin to GPIO Pin 15
+#
+# Programmer: Adrian Josele G. Quional
+
+# if passive buzzer is used, import the Speaker class from picozero
+from picozero import Speaker
+from time import sleep
+
+# creating a Speaker object
+speaker = Speaker(15)
+
+# continuously beep at 1 sec interval while the board has power
+# note: a passive buzzer can also be used to play different tones
+while True:
+    speaker.on()
+    sleep(1)
+    speaker.off()
+    sleep(1)
